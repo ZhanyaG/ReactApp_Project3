8a2b78ae4befff3d64020567a2526ab64415fe6f
ZhanyaG <kenez377@newschool.edu>
2025-11-03 16:43:19 -0500

    Update site

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ZhanyaG/', // 👈 must match your repo name
+  base: '/ReactApp_Project3/', // 👈 must match your repo name
 })
